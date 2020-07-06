db5eb7ecff5171d22d706debedab1f1acbbe0cb8
Zezombye <zezombye>
2020-07-06 06:12:20 +0200

    qsdf

--- generateAlphabetMappings.js
+++ generateAlphabetMappings.js
@@ -1,5 +1,4 @@
 
-
 `
 globalvar alphabet = [${Object.keys(mappings).map(x => "p'"+x+"'").join(", ")}]
 globalvar alphabetMappings = [${Object.values(mappings).map(mapping => "["+mapping.map(x => "p'"+x+"'").join(", ")+"]")}]
